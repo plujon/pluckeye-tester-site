a9c3b3887c3a17e2c6b5b93b26dc3a14b872817d
Jon Wilkes <jon@pluckeye.net>
2016-02-03 10:52:54 -0800

    Fun: Add ranks based on Titan.

--- helpers.rb
+++ helpers.rb
@@ -11,6 +11,45 @@ helpers do
     @auth.provided? and @auth.basic? and @auth.credentials and @auth.credentials == credentials
   end
 
+  def level(points)
+    # incr = 0
+    # 20.times.reduce([]) { |acc, x|
+    #   incr += 10
+    #   acc + [(acc.last || 0) + incr]
+    # }
+    levels = [10, 30, 60, 100, 150, 210, 280, 360, 450, 550, 660, 780, 910, 1050, 1200, 1360, 1530, 1710, 1900, 2100]
+    level = 0
+    levels.each do |x|
+      return level if points < x
+      level += 1
+    end
+    level
+  end
+
+  def rank(points)
+    [:centaur,
+     :ogre,
+     :gargoyle,
+     :lion,
+     :troll,
+     :cyclops,
+     :ranger,
+     :gorgon,
+     :minitaur,
+     :warbear,
+     :behemoth,
+     :unicorn,
+     :wyvern,
+     :griphon,
+     :giant,
+     :dragon,
+     :hydra,
+     :serpent,
+     :colossus,
+     :titan
+    ][level(points)]
+  end
+
   def link_to(o)
     case o
     when User
